07bb0f4e8c0e494e5a50bc86b2d4677c4b03ec70
manxiaqu <1847234666@qq.com>
2018-01-09 18:26:30 +0800

    Add invoke sync method to execute transaction
    Change some code to feat version 1.0.5;
    Add sync invoke method to execute transaction.

--- sdkservice/fabricsdk.go
+++ sdkservice/fabricsdk.go
@@ -1,101 +1,116 @@
 package sdkservice
 
 import (
-    "github.com/hyperledger/fabric-sdk-go/def/fabapi"
-    fab "github.com/hyperledger/fabric-sdk-go/api/apifabclient"
-    chmgmt "github.com/hyperledger/fabric-sdk-go/api/apitxn/chmgmtclient"
-    packager "github.com/hyperledger/fabric-sdk-go/pkg/fabric-client/ccpackager/gopackager"
-    resmgmt "github.com/hyperledger/fabric-sdk-go/api/apitxn/resmgmtclient"
-    cb "github.com/hyperledger/fabric-sdk-go/third_party/github.com/hyperledger/fabric/protos/common"
-    
-    "time"
-    "github.com/hyperledger/fabric-sdk-go/api/apitxn"
+	fab "github.com/hyperledger/fabric-sdk-go/api/apifabclient"
+	chmgmt "github.com/hyperledger/fabric-sdk-go/api/apitxn/chmgmtclient"
+	resmgmt "github.com/hyperledger/fabric-sdk-go/api/apitxn/resmgmtclient"
+	"github.com/hyperledger/fabric-sdk-go/def/fabapi"
+	packager "github.com/hyperledger/fabric-sdk-go/pkg/fabric-client/ccpackager/gopackager"
+	cb "github.com/hyperledger/fabric-sdk-go/third_party/github.com/hyperledger/fabric/protos/common"
+    "github.com/hyperledger/fabric-sdk-go/pkg/fabric-client/events"
+    "github.com/hyperledger/fabric-sdk-go/pkg/errors"
+    pb "github.com/hyperledger/fabric-sdk-go/third_party/github.com/hyperledger/fabric/protos/peer"
+    "github.com/hyperledger/fabric-sdk-go/pkg/logging"
+
+	"time"
+
+	"github.com/hyperledger/fabric-sdk-go/api/apitxn"
+    "fmt"
 )
 
+var logger = logging.NewLogger("sdkservice")
+
 const (
-    ADMIN = "Admin"
-    USER = "User"
+	ADMIN = "Admin"
+	USER  = "User"
 )
 
 // Supply service of connecting fabric peer/orderer to execute operations
 // sdk; channel; chaincode; user; org;
 type SDKService struct {
-    Options fabapi.Options
-    SDK *fabapi.FabricSDK
-    syClient fab.FabricClient
-    DefaultResMgmtClient resmgmt.ResourceMgmtClient
+	Options              fabapi.Options
+	SDK                  *fabapi.FabricSDK
+	syClient             fab.FabricClient
+	DefaultResMgmtClient resmgmt.ResourceMgmtClient
+	EventHub             fab.EventHub
 }
 
 // Get new sdk service instance
 func NewSDKService(configFile string) (*SDKService, error) {
-    sdkOptions := fabapi.Options{
-        ConfigFile: configFile,
-    }
-    
-    return &SDKService{
-        Options: sdkOptions,
-    }, nil
+	sdkOptions := fabapi.Options{
+		ConfigFile: configFile,
+	}
+
+	return &SDKService{
+		Options: sdkOptions,
+	}, nil
 }
 
 // Initialize sdk service
 // Use org and user to setup sdk
 func (sdkService *SDKService) Initialize(orgID, user string) error {
-    
-    sdk, err := fabapi.NewSDK(sdkService.Options)
-    if err != nil {
-        return err
+
+	sdk, err := fabapi.NewSDK(sdkService.Options)
+	if err != nil {
+		return err
+	}
+
+	sdkService.SDK = sdk
+
+	session, err := sdkService.SDK.NewPreEnrolledUserSession(orgID, user)
+	if err != nil {
+		return err
+	}
+
+	sc, err := sdk.NewSystemClient(session)
+	if err != nil {
+		return err
+	}
+	sdkService.syClient = sc
+	
+	if err := sdkService.setupEventHub(); err != nil {
+	    return err
     }
-    
-    session, err := sdkService.SDK.NewPreEnrolledUserSession(orgID, user)
-    if err != nil {
-        return err
-    }
-    
-    sc, err := sdk.NewSystemClient(session)
-    if err != nil {
-        return err
-    }
-    
-    sdkService.syClient = sc
-    
-    return nil
+
+	return nil
 }
 
 // Create channel and will let org join channel automatical
 func (sdkService *SDKService) CreateChannel(orderOrgName, orgName, channelConfig, channelID string) error {
-    
-    chMgmtClient, err := sdkService.SDK.NewChannelMgmtClientWithOpts(ADMIN, &fabapi.ChannelMgmtClientOpts{OrgName: orderOrgName})
-    if err != nil {
-        return err
-    }
-    
-    // Org admin user is signing user for creating channel
-    orgAdminUser, err := sdkService.SDK.NewPreEnrolledUser(orgName, ADMIN)
-    if err != nil {
-        return err
-    }
-    
-    req := chmgmt.SaveChannelRequest{ChannelID: channelID, ChannelConfig: channelConfig, SigningUser: orgAdminUser}
-    if err = chMgmtClient.SaveChannel(req); err != nil {
-        return err
-    }
-    
-    // TODO change to listen event
-    time.Sleep(time.Second * 3)
-    
-    // Resource management client is responsible for managing resources (joining channels, install/instantiate/upgrade chaincodes)
-    resMgmtClient, err := sdkService.SDK.NewResourceMgmtClient(ADMIN)
-    if err != nil {
-        return err
-    }
-    
-    if err = resMgmtClient.JoinChannel(channelID); err != nil {
-        return err
-    }
-    
-    sdkService.DefaultResMgmtClient = resMgmtClient
-    
-    return nil
+
+	chMgmtClient, err := sdkService.SDK.NewChannelMgmtClientWithOpts(ADMIN, &fabapi.ChannelMgmtClientOpts{OrgName: orderOrgName})
+	if err != nil {
+		return err
+	}
+
+	// Org admin user is signing user for creating channel
+	orgAdminUser, err := sdkService.SDK.NewPreEnrolledUser(orgName, ADMIN)
+	if err != nil {
+		return err
+	}
+
+	req := chmgmt.SaveChannelRequest{ChannelID: channelID, ChannelConfig: channelConfig, SigningUser: orgAdminUser}
+	// Save channel not return txID
+	if err = chMgmtClient.SaveChannel(req); err != nil {
+		return err
+	}
+	
+	// Wait for creating channel
+	time.Sleep(time.Second * 3)
+
+	// Resource management client is responsible for managing resources (joining channels, install/instantiate/upgrade chaincodes)
+	resMgmtClient, err := sdkService.SDK.NewResourceMgmtClient(ADMIN)
+	if err != nil {
+		return err
+	}
+
+	if err = resMgmtClient.JoinChannel(channelID); err != nil {
+		return err
+	}
+
+	sdkService.DefaultResMgmtClient = resMgmtClient
+
+	return nil
 }
 
 // TODO update channel config
@@ -104,61 +119,202 @@ func (sdkService *SDKService) UpdateChannel() {
 }
 
 func (sdkService *SDKService) InstallCC(ccID, ccPath, gopath, version string) error {
-    // Create chaincode package for example cc
-    // final chaincode path = gopath + "/src/" + ccPath
-    ccPkg, err := packager.NewCCPackage(ccPath, gopath)
-    if err != nil {
-        return err
-    }
-    
-    // Install example cc to org peers
-    installCCReq := resmgmt.InstallCCRequest{Name: ccID, Path: ccPath, Version: version, Package: ccPkg}
-    _, err = sdkService.DefaultResMgmtClient.InstallCC(installCCReq)
-    if err != nil {
-        return err
-    }
-    
-    return nil
+	// Create chaincode package for example cc
+	// final chaincode path = gopath + "/src/" + ccPath
+	ccPkg, err := packager.NewCCPackage(ccPath, gopath)
+	if err != nil {
+		return err
+	}
+
+	// Install example cc to org peers
+	installCCReq := resmgmt.InstallCCRequest{Name: ccID, Path: ccPath, Version: version, Package: ccPkg}
+
+	if sdkService.DefaultResMgmtClient == nil {
+		err := sdkService.SetDefaultResMgmtClient()
+		if err != nil {
+			return err
+		}
+	}
+	_, err = sdkService.DefaultResMgmtClient.InstallCC(installCCReq)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (sdkService *SDKService) InitializeCC(channelID, ccID, ccPath, version string, args [][]byte, ccPolicy *cb.SignaturePolicyEnvelope) error {
+
+	if sdkService.DefaultResMgmtClient == nil {
+		err := sdkService.SetDefaultResMgmtClient()
+		if err != nil {
+			return err
+		}
+	}
+
+	err := sdkService.DefaultResMgmtClient.InstantiateCC(channelID, resmgmt.InstantiateCCRequest{Name: ccID, Path: "chaincode_example02", Version: version, Args: args, Policy: ccPolicy})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (sdkService *SDKService) QueryCC(channelID, ccID, fcn string, args [][]byte) ([]byte, error) {
+	// Channel client is used to query and execute transactions
+	chClient, err := sdkService.SDK.NewChannelClient(channelID, "User1")
+	if err != nil {
+		return []byte{}, err
+	}
+
+	value, err := chClient.Query(apitxn.QueryRequest{ChaincodeID: ccID, Fcn: fcn, Args: args})
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return value, nil
+}
+
+// Invoke transaction with event to make sure the transaction is right
+// executed or not
+func (sdkService *SDKService) InvokeCC(channelID, ccID, fcn string, args [][]byte) (apitxn.TransactionID, bool, error) {
+
+	chClient, err := sdkService.SDK.NewChannelClient(channelID, "User1")
+	if err != nil {
+		return apitxn.TransactionID{}, false, err
+	}
+ 
+	txRes := make(chan apitxn.ExecuteTxResponse)
+    txOpts := apitxn.ExecuteTxOpts{Notifier: txRes}
     
-    err := sdkService.DefaultResMgmtClient.InstantiateCC(channelID, resmgmt.InstantiateCCRequest{Name: ccID, Path: "chaincode_example02", Version: version, Args: args, Policy: ccPolicy})
+	_, err = chClient.ExecuteTxWithOpts(apitxn.ExecuteTxRequest{ChaincodeID: ccID, Fcn: fcn, Args: args}, txOpts)
+	if err != nil {
+		return apitxn.TransactionID{}, false, err
+	}
+	
+	select {
+	case res := <- txRes:
+	   if res.Error != nil {
+	       return res.Response, false, res.Error
+       } else {
+           fmt.Println(res.TxValidationCode)
+           return res.Response, true, nil
+       }
+    }
+}
+
+func (sdkService *SDKService) InvokeCCAsync(channelID, ccID, fcn string, args [][]byte) (apitxn.TransactionID, error) {
+    
+    chClient, err := sdkService.SDK.NewChannelClient(channelID, "User1")
+    if err != nil {
+        return apitxn.TransactionID{}, err
+    }
+    
+    txnID, err := chClient.ExecuteTx(apitxn.ExecuteTxRequest{ChaincodeID: ccID, Fcn: fcn, Args: args})
+    if err != nil {
+        return apitxn.TransactionID{}, err
+    }
+    
+    return txnID, nil
+}
+
+func (sdkService *SDKService) setupEventHub() error {
+    eventHub, err := sdkService.getEventHub()
     if err != nil {
         return err
     }
     
+    if err := eventHub.Connect(); err != nil {
+        return errors.WithMessage(err, "eventHub connect failed")
+    }
+    sdkService.EventHub = eventHub
+    
     return nil
 }
 
-func (sdkService *SDKService) QueryCC(channelID, ccID, fcn string, args [][]byte) ([]byte, error) {
-    // Channel client is used to query and execute transactions
-    chClient, err := sdkService.SDK.NewChannelClient(channelID, "User1")
+func (sdkService *SDKService) getEventHub() (fab.EventHub, error) {
+    
+    session, err := sdkService.SDK.NewPreEnrolledUserSession("org1", "Admin")
     if err != nil {
-        return []byte{}, err
+        return nil, err
     }
     
-    value, err := chClient.Query(apitxn.QueryRequest{ChaincodeID: ccID, Fcn: fcn, Args: args})
+    sc, err := sdkService.SDK.NewSystemClient(session)
     if err != nil {
-        return []byte{}, err
+        return nil, err
     }
     
-    return value, nil
+    eventHub, err := events.NewEventHub(sc)
+    if err != nil {
+        return nil, errors.WithMessage(err, "NewEventHub failed")
+    }
+    
+    foundEventHub := false
+    peerConfig, err := sdkService.syClient.Config().PeersConfig("org1")
+    if err != nil {
+        return nil, errors.WithMessage(err, "PeersConfig failed")
+    }
+    
+    for _, p := range peerConfig {
+        if p.URL != "" {
+            //("EventHub connect to peer (%s)", p.URL)
+            serverHostOverride := ""
+            if str, ok := p.GRPCOptions["ssl-target-name-override"].(string); ok {
+                serverHostOverride = str
+            }
+            eventHub.SetPeerAddr(p.EventURL, p.TLSCACerts.Path, serverHostOverride)
+            foundEventHub = true
+            break
+        }
+    }
+    
+    if !foundEventHub {
+        return nil, errors.New("event hub configuration not found")
+    }
+    
+    return eventHub, nil
 }
 
-func (sdkService *SDKService) InvokeCC(channelID, ccID, fcn string, args [][]byte) (apitxn.TransactionID, error) {
+// RegisterTxEvent registers on the given eventhub for the give transaction
+// returns a boolean channel which receives true when the event is complete
+// and an error channel for errors
+func (sdkService *SDKService) registerTxEvent(txID apitxn.TransactionID) (chan bool, chan error) {
+    done := make(chan bool)
+    fail := make(chan error)
     
-    chClient, err := sdkService.SDK.NewChannelClient(channelID, "User1")
-    if err != nil {
-        return apitxn.TransactionID{}, err
-    }
+    // This may happended after the exec of transaction, and will never get
+    // Change to use executeTxWithOpts
+    sdkService.EventHub.RegisterTxEvent(txID, func(txId string, errorCode pb.TxValidationCode, err error) {
+        if err != nil {
+            logger.Debugf("Received error event for txid(%s)", txId)
+            fail <- err
+        } else {
+            logger.Debugf("Received success event for txid(%s)", txId)
+            done <- true
+        }
+    })
     
-    value, err := chClient.ExecuteTx(apitxn.ExecuteTxRequest{ChaincodeID: ccID, Fcn: fcn, Args: args})
-    if err != nil {
-        return apitxn.TransactionID{}, err
-    }
-    
-    return value, nil
+    return done, fail
 }
 
+func (sdkService *SDKService) GetDefaultResMgmtClient() (resmgmt.ResourceMgmtClient, error) {
 
+	resMgmtClient, err := sdkService.SDK.NewResourceMgmtClient(ADMIN)
+	if err != nil {
+		return nil, err
+	}
+
+	return resMgmtClient, nil
+}
+
+func (sdkService *SDKService) SetDefaultResMgmtClient() error {
+
+	client, err := sdkService.GetDefaultResMgmtClient()
+	if err != nil {
+		return err
+	}
+
+	sdkService.DefaultResMgmtClient = client
+
+	return nil
+}
--- sdkservice/fabricsdk_test.go
+++ sdkservice/fabricsdk_test.go
@@ -1,22 +1,22 @@
 package sdkservice
 
 import (
-    "github.com/hyperledger/fabric-sdk-go/third_party/github.com/hyperledger/fabric/common/cauthdsl"
-    "testing"
-    "strconv"
+	"strconv"
+	"testing"
+
+	"github.com/hyperledger/fabric-sdk-go/third_party/github.com/hyperledger/fabric/common/cauthdsl"
 )
 
 const (
-    basePath = "/home/ubuntu/gopath/src/github.com/hyperledger/fabric-manage-go/"
-    configFilePath = "configFile.yaml"
-    orgName = "Org1"
-    orgAdmin = "Admin"
-    channelID = "mychannel"
-    channelConfigPath = "e2e_cli/channel-artifacts/channel.tx"
-    ccID = "mycc"
-    ccPath = "chaincode_example02"
-    gopath = basePath + "/test/"
-    version = "0"
+	basePath          = "/home/ubuntu/gopath/src/github.com/hyperledger/fabric-test-go/"
+	configFilePath    = "configFile.yaml"
+	orgName           = "Org1"
+	channelID         = "mychannel"
+	channelConfigPath = "e2e_cli/channel-artifacts/channel.tx"
+	ccID              = "mycc"
+	ccPath            = "chaincode_example02"
+	gopath            = basePath + "/test/"
+	version           = "0"
 )
 
 var initArgs = [][]byte{[]byte("init"), []byte("a"), []byte("10"), []byte("b"), []byte("20")}
@@ -25,93 +25,100 @@ var queryAArgs = [][]byte{[]byte("a")}
 var queryBArgs = [][]byte{[]byte("b")}
 
 func TestSDKService_CreateChannel(t *testing.T) {
-    sdkService, err := NewSDKService(basePath + configFilePath)
-    if err != nil {
-        t.Fatalf("Get new sdkService failed")
-    }
-    
-    orderOrg := "orderer"
-    orderUser := "Admin"
-    
-    sdkService.Initialize(orderOrg, orderUser)
-    
-    err = sdkService.CreateChannel(orderOrg, orgName, basePath + channelConfigPath, channelID)
-    if err != nil {
-        t.Fatalf("CreateChannel failed")
-    }
+	sdkService, err := NewSDKService(basePath + configFilePath)
+	if err != nil {
+		t.Fatalf("Get new sdkService failed")
+	}
+
+	orderOrg := "ordererorg"
+	orderUser := "Admin"
+
+	sdkService.Initialize(orderOrg, orderUser)
+
+	err = sdkService.CreateChannel(orderOrg, orgName, basePath+channelConfigPath, channelID)
+	if err != nil {
+		t.Fatalf("CreateChannel failed: %s", err.Error())
+	}
 }
 
 func TestSDKService_InstallCC(t *testing.T) {
-    
-    sdkService, err := NewSDKService(basePath + configFilePath)
-    if err != nil {
-        t.Fatalf("Get new sdkService failed")
-    }
-    
-    orderOrg := "orderer"
-    orderUser := "Admin"
-    
-    sdkService.Initialize(orderOrg, orderUser)
-    
-    err = sdkService.InstallCC(ccID, ccPath, gopath, version)
-    if err != nil {
-        t.Fatalf("Failed to install chaincode")
-    }
+
+	sdkService, err := NewSDKService(basePath + configFilePath)
+	if err != nil {
+		t.Fatalf("Get new sdkService failed")
+	}
+
+	orderOrg := "ordererorg"
+	orderUser := "Admin"
+
+	sdkService.Initialize(orderOrg, orderUser)
+
+	err = sdkService.InstallCC(ccID, ccPath, gopath, version)
+	if err != nil {
+		t.Fatalf("Failed to install chaincode")
+	}
 }
 
 func TestSDKService_InitializeCC(t *testing.T) {
-    sdkService, err := NewSDKService(basePath + configFilePath)
-    if err != nil {
-        t.Fatalf("Get new sdkService failed")
-    }
-    
-    orderOrg := "orderer"
-    orderUser := "Admin"
-    
-    sdkService.Initialize(orderOrg, orderUser)
-    
-    ccPolicy := cauthdsl.SignedByAnyMember([]string{"Org1MSP"})
-    err = sdkService.InitializeCC(channelID, ccID, ccPath, version, initArgs, ccPolicy)
-    if err != nil {
-        t.Fatalf("Failed to initializeCC")
-    }
+	sdkService, err := NewSDKService(basePath + configFilePath)
+	if err != nil {
+		t.Fatalf("Get new sdkService failed")
+	}
+
+	orderOrg := "orderer"
+	orderUser := "Admin"
+
+	sdkService.Initialize(orderOrg, orderUser)
+
+	ccPolicy := cauthdsl.SignedByAnyMember([]string{"Org1MSP"})
+	err = sdkService.InitializeCC(channelID, ccID, ccPath, version, initArgs, ccPolicy)
+	if err != nil {
+		t.Fatalf("Failed to initializeCC")
+	}
 }
 
 func TestSDKService_QueryCC(t *testing.T) {
-    sdkService, err := NewSDKService(basePath + configFilePath)
-    if err != nil {
-        t.Fatalf("Get new sdkService failed")
-    }
-    
-    orderOrg := "orderer"
-    orderUser := "Admin"
-    
-    sdkService.Initialize(orderOrg, orderUser)
-    
-    value, err := sdkService.QueryCC(channelID, ccID, "query", queryAArgs)
-    if err != nil {
-        t.Fatalf("Failed to query ")
-    }
-    
-    valueInt, _ := strconv.Atoi(string(value))
-    if valueInt != 10 {
-        t.Fatalf("Query reuslt is not equal")
-    }
+	sdkService, err := NewSDKService(basePath + configFilePath)
+	if err != nil {
+		t.Fatalf("Get new sdkService failed")
+	}
+
+	orderOrg := "ordererorg"
+	orderUser := "Admin"
+
+	sdkService.Initialize(orderOrg, orderUser)
+
+	value, err := sdkService.QueryCC(channelID, ccID, "query", queryAArgs)
+	if err != nil {
+		t.Fatalf("Failed to query ")
+	}
+
+	valueInt, _ := strconv.Atoi(string(value))
+	if valueInt != 10 {
+		t.Fatalf("Query reuslt is not equal")
+	}
 }
 
 func TestSDKService_InvokeCC(t *testing.T) {
-    sdkService, err := NewSDKService(basePath + configFilePath)
-    if err != nil {
-        t.Fatalf("Get new sdkService failed")
-    }
-    
-    orderOrg := "orderer"
-    orderUser := "Admin"
-    
-    sdkService.Initialize(orderOrg, orderUser)
-    
-    _, err = sdkService.InvokeCC(channelID, ccID, "invoke", invokeArgs)
-    if err != nil {
-        t.Fatalf("Failed to invoke transaction")
-    }
+	sdkService, err := NewSDKService(basePath + configFilePath)
+	if err != nil {
+		t.Fatalf("Get new sdkService failed")
+	}
+
+	orderOrg := "ordererorg"
+	orderUser := "Admin"
+
+	err = sdkService.Initialize(orderOrg, orderUser)
+	if err != nil {
+		t.Fatalf("Failed to initialize : %s", err.Error())
+	}
+	
+	_, execRight, err := sdkService.InvokeCC(channelID, ccID, "invoke", invokeArgs)
+	if err != nil {
+		t.Fatalf("Failed to invoke transaction")
+	}
+	
+	if !execRight {
+		t.Fatalf("Failed to invoke transaction rightly")
+	}
 }
